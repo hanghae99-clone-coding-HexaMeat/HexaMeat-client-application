f374125e36e695e179e475d4e788657350e0f333
1117JHH <74212632+janghwahyun@users.noreply.github.com>
2021-07-21 15:46:16 +0900

    add emptycart

--- src/shared/App.js
+++ src/shared/App.js
@@ -5,14 +5,15 @@ import Detail from "../pages/Detail";
 import Login from "../pages/Login";
 import Signup from "../pages/Signup";
 import Header from "../components/Header";
+import Shopping from "../pages/Shopping";
+import Cart from "../pages/Cart";
+import EmptyCart from "../components/EmptyCart";
 
 import { Route } from "react-router-dom";
 import { ConnectedRouter } from "connected-react-router";
 import { history } from "../redux/configureStore";
 
 import { Grid } from "../elements";
-import Shopping from "../pages/Shopping";
-import Cart from "../pages/Cart";
 
 function App() {
   return (
@@ -26,6 +27,7 @@ function App() {
           <Route path="/login" exacrt component={Login} />
           <Route path="/signup" exacrt component={Signup} />
           <Route path="/cart" exact component={Cart} />
+          <Route path="/cart/:id" exact component={EmptyCart} />
         </ConnectedRouter>
       </Grid>
     </React.Fragment>
